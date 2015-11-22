0fd08fb7f36328d2909df82ae97c6259f453cba3
Tim Davis <thebearingedge@gmail.com>
2015-11-22 15:56:57 -0800

    first set

---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+import chai from 'chai'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+import chaiAsPromised from 'chai-as-promised'
+import jsom from 'jsdom'
+import rootRequire from 'root-require'
+
+chai.use(sinonChai)
+chai.use(chaiAsPromised)
+
+const { expect } = chai
+const { spy, stub } = sinon
+
+export {
+  chai, expect, sinon, spy, stub, jsdom, rootRequire,
+}
