4108d08d08c3c244168f6781ac4cfb8f343c1ae8
Tim Davis <thebearingedge@gmail.com>
2015-11-22 16:12:13 -0800

    remove root-require

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
 import chaiAsPromised from 'chai-as-promised'
 import jsdom from 'jsdom'
-import rootRequire from 'root-require'
 
 chai.use(sinonChai)
 chai.use(chaiAsPromised)
@@ -13,5 +12,5 @@ const { expect } = chai
 const { spy, stub } = sinon
 
 export {
-  chai, expect, sinon, spy, stub, jsdom, rootRequire,
+  chai, expect, sinon, spy, stub, jsdom,
 }
