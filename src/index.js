9b835c784904e3ab6caff84d005bc21b8a9785f4
Tim Davis <thebearingedge@gmail.com>
2015-11-29 14:17:44 -0800

    add chai-http

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import chai from 'chai'
 import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
 import chaiAsPromised from 'chai-as-promised'
+import chaiHttp from 'chai-http'
 import jsdom from 'jsdom'
 
 chai.use(sinonChai)
 chai.use(chaiAsPromised)
+chai.use(chaiHttp)
 
 require('sinon-as-promised')
 
-const { expect } = chai
+const { expect, request } = chai
 const { spy, stub } = sinon
 
 export {
-  chai, expect, sinon, spy, stub, jsdom,
+  chai, expect, request, sinon, spy, stub, jsdom,
 }
